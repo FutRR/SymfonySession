c76b6239bb0e81f9fcf7444c6a3883b501409992
FutRR <futterermaxime@gmail.com>
2024-04-24 14:51:39 +0200

    added stagiaire create/update form

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +22,35 @@ class StagiaireController extends AbstractController
             'stagiaires' => $stagiaires,
         ]);
     }
+
+    #[Route('/stagiaire/new', name: 'new_stagiaire')]
+    #[Route('/stagiaire/{id}/edit', name: 'edit_stagiaire')]
+    public function new_edit(Stagiaire $stagiaire = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$stagiaire) {
+            $stagiaire = new Stagiaire();
+        }
+
+        $form = $this->createForm(StagiaireType::class, $stagiaire);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $stagiaire = $form->getData();
+            $entityManager->persist($stagiaire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_stagiaire');
+        }
+
+        return $this->render("stagiaire/new.html.twig", [
+            'formAddStagiaire' => $form,
+            'edit' => $stagiaire->getId()
+        ]);
+
+    }
+
 
     #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
     public function show(Stagiaire $stagiaire): Response
---
+++ src/Form/StagiaireType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+
+class StagiaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomStagiaire', TextType::class, [
+                'label' => 'Nom : ',
+                "attr" => [
+                    'class' => "form"
+                ]
+            ])
+            ->add('prenomStagiaire', TextType::class, [
+                'label' => 'Prenom : ',
+                "attr" => [
+                    'class' => "form"
+                ]
+            ])
+            ->add('emailStagiaire', TextType::class, [
+                'label' => 'Email : ',
+                "attr" => [
+                    'class' => "form"
+                ]
+            ])
+            ->add('dateNaissance', BirthdayType::class, [
+                'label' => 'Date de naissance :',
+                "attr" => [
+                    'class' => "form date"
+                ],
+                'format' => 'dd-MM-yyyy'
+            ])
+            ->add('telephone', TextType::class, [
+                'label' => 'Téléphone',
+                "attr" => [
+                    'class' => "form"
+                ]
+            ])
+            ->add('ville', TextType::class, [
+                'label' => 'Ville',
+                "attr" => [
+                    'class' => "form"
+                ]
+            ])
+            ->add('valider', SubmitType::class, [
+                "attr" => [
+                    'class' => "submit btn"
+                ]
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stagiaire::class,
+        ]);
+    }
+}
