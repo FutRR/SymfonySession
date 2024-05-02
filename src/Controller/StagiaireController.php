0e4ae647be467636b716c456ae0b53c8221b0c24
FutRR <futterermaxime@gmail.com>
2024-05-02 15:50:57 +0200

    added stagiaire delete

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -51,6 +51,13 @@ class StagiaireController extends AbstractController
 
     }
 
+    #[Route('/stagiaire/{id}/delete', name: 'delete_stagiaire')]
+    public function delete(Stagiaire $stagiaire, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($stagiaire);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_stagiaire');
+    }
 
     #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
     public function show(Stagiaire $stagiaire): Response
