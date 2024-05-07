6dfcae384a5baeca6a92a5137de71189cc62518a
FutRR <futterermaxime@gmail.com>
2024-05-07 12:02:52 +0200

    added dark/light mode toggle w/ localStorage support

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,60 @@
-const menuburger = document.querySelector(".menuburger");
-const navlinks = document.querySelector(".links");
-const main = document.querySelector("main");
-const logo = document.querySelector(".logo");
+document.addEventListener("DOMContentLoaded", function () {
+  // BURGER MENU //
+  const menuburger = document.querySelector(".menuburger");
+  const navlinks = document.querySelector(".links");
+  const main = document.querySelector("main");
+  const logo = document.querySelector(".logo");
 
-function menuChange(x) {
-  x.classList.toggle("change");
-  navlinks.classList.toggle("mobile-menu");
-  main.classList.toggle("blur");
-  logo.classList.toggle("blur");
-}
+  function menuChange(x) {
+    x.classList.toggle("change");
+    navlinks.classList.toggle("mobile-menu");
+    main.classList.toggle("blur");
+    logo.classList.toggle("blur");
+  }
 
-// menuburger.addEventListener("click", () => {
-//   navlinks.classList.toggle("mobile-menu");
-//   main.classList.toggle("blur");
-//   logo.classList.toggle("blur");
-// });
+  // SESSION FORM TOGGLE //
 
-const moduleToggle = document.querySelector("#toggle-module");
-const moduleForm = document.querySelector("#module-form");
+  const moduleToggle = document.querySelector("#toggle-module");
+  const moduleForm = document.querySelector("#module-form");
 
-moduleToggle.addEventListener("click", () => {
-  moduleForm.classList.toggle("form-toggle");
-});
+  if (moduleToggle) {
+    moduleToggle.addEventListener("click", () => {
+      moduleForm.classList.toggle("form-toggle");
+    });
+  }
 
-const stagiaireToggle = document.querySelector("#toggle-stagiaire");
-const stagiaireForm = document.querySelector("#stagiaire-form");
+  const stagiaireToggle = document.querySelector("#toggle-stagiaire");
+  const stagiaireForm = document.querySelector("#stagiaire-form");
 
-stagiaireToggle.addEventListener("click", () => {
-  stagiaireForm.classList.toggle("form-toggle");
+  if (stagiaireForm) {
+    stagiaireToggle.addEventListener("click", () => {
+      stagiaireForm.classList.toggle("form-toggle");
+    });
+  }
+
+  // DARK/LIGHT MODE TOGGLE
+  var themeToggle = document.getElementById("theme-toggle");
+  var body = document.body;
+  var currentTheme = localStorage.getItem("theme") || "light";
+  console.log("test1");
+  setTheme(currentTheme);
+
+  function setTheme(theme) {
+    // save to localStorage
+    localStorage.setItem("theme", theme);
+    body.setAttribute("data-theme", theme);
+    themeToggle.innerHTML =
+      theme === "light"
+        ? "<i class='fa-regular fa-moon'></i>"
+        : "<i class='fa-regular fa-sun'></i>";
+    console.log("test2");
+  }
+
+  function toggleTheme() {
+    var newTheme =
+      body.getAttribute("data-theme") === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    console.log("test3");
+  }
+  themeToggle.addEventListener("click", toggleTheme);
 });
